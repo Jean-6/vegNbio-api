ae58978ee13e3bd04eb25bdf1a71d9aa9cc6a565
Jean <oka.jeanjaures@gmail.com>
2025-12-04 14:56:09 +0100

    declare dto to send authentication response

--- src/main/java/org/example/vegnbioapi/dto/LoginResponse.java
+++ src/main/java/org/example/vegnbioapi/dto/LoginResponse.java
@@ -15,6 +15,8 @@ public class LoginResponse {
     private String username;
     private String email;
     private List<Role> roles;
+    private boolean isActive = true;
+    //private boolean isVerified = false;
     private  String token;
 
 }
